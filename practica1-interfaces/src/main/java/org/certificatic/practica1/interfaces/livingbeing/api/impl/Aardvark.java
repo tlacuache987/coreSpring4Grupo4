4160debd827f5464f14f980fddac27d73f3bcc40
Ivan Garcia <isc.ivgarcia@gmail.com>
2017-04-22 13:27:57 -0500

    Practica 1 - Modelo objetos LivingBeing

--- practica1-interfaces/src/main/java/org/certificatic/practica1/interfaces/livingbeing/api/impl/Aardvark.java
+++ practica1-interfaces/src/main/java/org/certificatic/practica1/interfaces/livingbeing/api/impl/Aardvark.java
@@ -1,6 +1,30 @@
 package org.certificatic.practica1.interfaces.livingbeing.api.impl;
 
-public class Aardvark {
+import org.certificatic.practica1.interfaces.livingbeing.api.BugEater;
 
-	// Implementar
+public class Aardvark extends Animal implements BugEater {
+
+	public Aardvark(String subType) {
+		this.setSubType(subType);
+	}
+
+	public void born() {
+		System.out.println("born like a " + this.getType() + " (" + this.getSubType() + ")");
+	}
+
+	public void grow() {
+		System.out.println("grow like a " + this.getType() + " (" + this.getSubType() + ")");
+	}
+
+	public void breed() {
+		System.out.println("breed like a " + this.getType() + " (" + this.getSubType() + ")");
+	}
+
+	public void die() {
+		System.out.println("die like a " + this.getType() + " (" + this.getSubType() + ")");
+	}
+
+	public void eatBug() {
+		System.out.println("eating bug like a " + this.getType() + " (" + this.getSubType() + ")");
+	}
 }
--- practica1-interfaces/src/main/java/org/certificatic/practica1/interfaces/livingbeing/api/impl/VenusFlyTrap.java
+++ practica1-interfaces/src/main/java/org/certificatic/practica1/interfaces/livingbeing/api/impl/VenusFlyTrap.java
@@ -1,5 +1,40 @@
 package org.certificatic.practica1.interfaces.livingbeing.api.impl;
 
-public class VenusFlyTrap {
-	// Implementar
+import org.certificatic.practica1.interfaces.livingbeing.api.BugEater;
+
+public class VenusFlyTrap extends Plant implements BugEater{
+	
+	public VenusFlyTrap(String subType) {
+		this.setSubType(subType);
+	}
+
+	@Override
+	public void born() {
+		System.out.println("born like a " + this.getType() + 
+				" (" + this.getSubType() + ")");
+	}
+
+	@Override
+	public void grow() {
+		System.out.println("grow like a " + this.getType() + 
+				" (" + this.getSubType() + ")");
+	}
+
+	@Override
+	public void breed() {
+		System.out.println("breed like a " + this.getType() + 
+				" (" + this.getSubType() + ")");
+	}
+
+	@Override
+	public void die() {
+		System.out.println("die like a " + this.getType() + 
+				" (" + this.getSubType() + ")");
+	}
+
+	@Override
+	public void eatBug() {
+		System.out.println("eat bug like a " + this.getType() + 
+				" (" + this.getSubType() + ")");
+	}
 }
